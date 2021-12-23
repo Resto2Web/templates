024179b95b5cbe4939a159d9e6bf1987d64cdc00
WhereIsLucas <lucas@retinens.com>
2021-12-23 09:21:03 +0100

    wip

--- resources/views/template1/components/home/hero.blade.php
+++ resources/views/template1/components/home/hero.blade.php
@@ -1,34 +1,24 @@
 
 <div class="swiper-container swiper-hero">
     <div class="swiper-wrapper">
-        <div class="swiper-slide  ">
-            <div class="d-flex justify-content-center align-items-center " style="background-image: linear-gradient(rgba(0,0,0,0.2),rgba(0,0,0,0.2)),url(http://placehold.it/1920x1080);
+        @foreach ($homeSlides as $homeSlide)
+            <div class="swiper-slide  ">
+                <div class="d-flex justify-content-center align-items-center " style="background-image: linear-gradient(rgba(0,0,0,0.2),rgba(0,0,0,0.2)),url({{ $homeSlide->getFirstMediaUrl('image') }});
                 background-position: center;
                 height: 80vh;
                 -webkit-background-size: cover;
                 -moz-background-size: cover;
                 background-size: cover;
                 -o-background-size: cover;">
-                <div class="text-white text-center">
-                    <h1 class=" ">Restaurant</h1>
-                    <h2 class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, earum!</h2>
+                    <div class="text-white text-center">
+                        <h1 class=" ">{{ $homeSlide->title }}</h1>
+                        @if ($homeSlide->subtitle)
+                            <h2 class="">{{ $homeSlide->subtitle }}</h2>
+                        @endif
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="swiper-slide  ">
-            <div class="hero-home d-flex justify-content-center align-items-center " style="background-image: linear-gradient(rgba(0,0,0,0.2),rgba(0,0,0,0.2)),url(https://placehold.it/1920x1080);
-                background-position: center;
-                height: 80vh;
-                -webkit-background-size: cover;
-                -moz-background-size: cover;
-                background-size: cover;
-                -o-background-size: cover;" >
-                <div class="text-white text-center">
-                    <h1 class=" ">Restaurant</h1>
-                    <h2 class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, earum!</h2>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
     <div class="swiper-pagination"></div>
 
