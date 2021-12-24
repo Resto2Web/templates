77ae53cb2dafc0a6e8b2c4d72ee97a6c8f33a8ba
Hugo <trevisani.hugo@gmail.com>
2021-12-24 10:58:13 +0400

    wip

---
+++ resources/views/components/home/menu-3-categories.blade.php
@@ -0,0 +1,28 @@
+<div class="bg-dark">
+    <div class="py-7 container">
+        <h2 class="h1 text-white text-center">Lorem ipsum dolor sit amet.</h2>
+        <div class="row justify-content-center text-white mt-5">
+            <div class="col-lg-4">
+                <div class=" text-center">
+                    <img src="http://placehold.it/250x250" alt="" class="img-fluid">
+                    <h4 class=" fw-bold mt-3">Lorem ipsum dolor.</h4>
+                    <p class="text-muted mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum explicabo id molestiae nemo nihil nostrum?</p>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class=" text-center">
+                    <img src="http://placehold.it/250x250" alt="" class="img-fluid">
+                    <h4 class=" fw-bold mt-3">Lorem ipsum dolor.</h4>
+                    <p class="text-muted mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque explicabo labore quae quidem sunt ut!</p>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class=" text-center">
+                    <img src="http://placehold.it/250x250" alt="" class="img-fluid">
+                    <h4 class=" fw-bold mt-3">Lorem ipsum dolor.</h4>
+                    <p class="text-muted mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi ea expedita itaque necessitatibus nisi ut.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
